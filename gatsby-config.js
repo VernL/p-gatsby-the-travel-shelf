5f26740f06b7ec890c146a59224e15fadeff4bbe
VernL <vernon.lillies@gmail.com>
2018-03-24 00:05:37 -0400

    feat: add and configure image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,15 +12,23 @@ module.exports = {
           path: `${__dirname}/src/`,
       }
     },
-    `gatsby-transformer-remark`
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //       name: `data`,
-    //       path: `${__dirname}/src/data/`,
-    //   },
-    // },
-    // `gatsby-transformer-sharp`,
-    // `gatsby-plugin-sharp`,
+    `gatsby-transformer-remark`,
+      `gatsby-plugin-sharp`,
+      {
+          resolve: `gatsby-transformer-remark`,
+          options: {
+              plugins: [
+                  {
+                      resolve: `gatsby-remark-images`,
+                      options: {
+                          // It's important to specify the maxWidth (in pixels) of
+                          // the content container as this plugin uses this as the
+                          // base for generating different widths of each image.
+                          maxWidth: 590,
+                      },
+                  },
+              ],
+          },
+      },
   ]
 };
